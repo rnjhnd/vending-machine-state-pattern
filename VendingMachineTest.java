7a33b2f74f3af58eaed974c7b73b6eabe86b48c7
Aren John Esoen <152838648+ArenJohnD@users.noreply.github.com>
2025-02-21 17:26:53 +0000

    Save Files

--- VendingMachineTest.java
+++ VendingMachineTest.java
@@ -1,6 +1,6 @@
 public class VendingMachineTest {
     public static void main(String[] args) {
-        VendingMachine vendingMachine = new VendingMachine(5);
+        VendingMachine vendingMachine = new VendingMachine(10);
 
         vendingMachine.selectItem("Soda");
         vendingMachine.insertCoin(50);
